2fbb0609c6bc738febb2b164ca0dc27af78c8d0f
benjyms <87244593+benjyms@users.noreply.github.com>
2021-10-24 17:28:32 -0400

    flask and mongo progress saved

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -12,6 +12,36 @@ mongo = PyMongo(app)
 # or set inline
  # mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
+
 @app.route("/")
-def index():
-    # pick up here when ready
+def home():
+
+    # Return template
+    return render_template("index.html")
+
+
+# Route that will trigger the scrape function
+@app.route("/scrape")
+def scrape():
+
+    # Run the scrape function and save the results to a variable
+    scraped_data = scrape_mars.scrape()
+
+    # Update the Mongo database using update and upsert=True
+    mars_data.update({}, scraped_data, upsert=True)
+
+    # Redirect to the scraped data page
+    return redirect("/data")
+
+# Route to render data.html template using data from Mongo
+@app.route("/data")
+def data():
+
+    # Find one record of data from the mongo database
+    mars_info = mongo.db.mars_data.find_one()
+
+    # Return template and data
+    return render_template("data.html", info=mars_info)
+
+if __name__ == "__main__":
+    app.run(debug=True)
